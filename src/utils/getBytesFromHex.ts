7678f1fc19dd44c6cb8bf6ff5705a1edb6728adf
Stanislav Kalashnik <darkpark.main@gmail.com>
2023-04-05 13:40:20 +0300

    expose ts and utils

--- src/utils/getBytesFromHex.ts
+++ src/utils/getBytesFromHex.ts
@@ -1,6 +1,8 @@
 /**
  * Convert hex string to byte array.
  *
+ * @param hex - input bytes hex representation
+ *
  * @example
  * input: '02 05 0c ff 69 8b 7d' or '02050cff698b7d' or ' 0x23 0xaa 0x00'
  * output: [2, 5, 12, 255, 105, 139, 125]
