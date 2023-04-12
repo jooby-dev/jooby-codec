33a2535cb6810c829b982cf21ee3ec28cf526b14
Stanislav Kalashnik <darkpark.main@gmail.com>
2023-04-12 10:11:58 +0300

    add bitSet

---
+++ src/utils/bitSet.ts
@@ -0,0 +1,33 @@
+export type TBitSetMask = Record<string, number>;
+
+export type TBooleanObject = Record<string, boolean>;
+
+
+/**
+ * Get integer number from object with value values according to the given bit mask.
+ */
+export const fromObject = ( bitMask: TBitSetMask, booleanObject: TBooleanObject ): number => {
+    let result = 0;
+
+    for ( const [name, value] of Object.entries(booleanObject) ) {
+        if ( name in bitMask && value ) {
+            result |= bitMask[name];
+        }
+    }
+
+    return result;
+};
+
+
+/**
+ * Get object with value values from integer number according to the given bit mask.
+ */
+export const toObject = ( bitMask: TBitSetMask, value: number ): TBooleanObject => {
+    const result: TBooleanObject = {};
+
+    for ( const [name, position] of Object.entries(bitMask) ) {
+        result[name] = (value & position) !== 0;
+    }
+
+    return result;
+};
