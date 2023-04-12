9fd8a8ea7a2bf00ce6e93b1aae51127101c9664e
Igor Zaporozhets <i.zaporozhets@infomir.com>
2023-04-12 17:53:00 +0300

    add docs for device parameters

--- src/constants/deviceParameters.ts
+++ src/constants/deviceParameters.ts
@@ -2,6 +2,17 @@
  * Device parameters. Used in SetParameter/GetParameter.
  */
 
+/**
+ * Setup initial data for device, pulse counter, pulse coefficient, meter value
+ */
 export const INITIAL_DATA = 23;
+
+/**
+ * Status of device data sending, absolute value or pulse counter
+ */
 export const ABSOLUTE_DATA_STATUS = 24;
+
+/**
+ * same as INITIAL_DATA but with channel number
+ */
 export const INITIAL_DATA_MULTI_CHANNEL = 29;
