761853637cd89f052f83427d5039ae5d969b5511
DarkPark <darkpark.main@gmail.com>
2023-04-19 17:54:14 +0300

    add branding to TTime2000 (#53)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,15 +1,19 @@
 /**
- * Time 2000
+ * Helper to create special branded types from primitives.
  *
+ * https://medium.com/@KevinBGreene/surviving-the-typescript-ecosystem-branding-and-type-tagging-6cf6e516523d
+ */
+type BrandType<K, T> = K & {info?: T};
+
+/**
  * Seconds since year 2000.
  *
  * @example
  * Time `2023-04-03T14:01:17.000Z` is `1680530477` in Unix time representation.
  * Unix time for year `2000` is `946684800` so seconds since year `2000` is `1680530477` - `946684800` = `733845677`.
- *
- * [Full documentation](https://github.com/jooby-dev/jooby-docs/blob/main/docs/types.md#time-2000)
  */
-export type TTime2000 = number;
+export type TTime2000 = BrandType<number, 'seconds'>;
+
 
 const INITIAL_YEAR_TIMESTAMP = 946684800000;
 const MILLISECONDS_IN_SECONDS = 1000;
