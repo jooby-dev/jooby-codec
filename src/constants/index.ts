4c21258e9998da7bcf09a068a307a0e5b8d05b5b
Stanislav Kalashnik <darkpark.main@gmail.com>
2023-04-20 10:24:59 +0300

    fix public interface for directions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export * as directionTypes from './directions.js';
+export * as directions from './directions.js';
 export * as events from './events.js';
 export * as hardwareTypes from './hardwareTypes.js';
