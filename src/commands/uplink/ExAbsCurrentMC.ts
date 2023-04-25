e9a5329059d86b922dbe2ef666e11feaa5ae4366
DeadByElpy <deadbyelpy@gmail.com>
2023-04-25 17:43:21 +0300

    add docs and examples for uplink commands

--- src/commands/uplink/ExAbsCurrentMC.ts
+++ src/commands/uplink/ExAbsCurrentMC.ts
@@ -1,4 +1,4 @@
-import Command from '../../Command.js';
+import Command, {TCommandExampleList} from '../../Command.js';
 import CommandBinaryBuffer, {IChannelAbsoluteValue} from '../../CommandBinaryBuffer.js';
 import {UPLINK} from '../../constants/directions.js';
 
@@ -7,14 +7,59 @@ interface IExAbsCurrentMCParameters {
     channelList: Array<IChannelAbsoluteValue>
 }
 
-
 const COMMAND_ID = 0x0f1f;
 
 // channelList 3 byte (max channelList: 14)
 // 3 + (14 * (1 byte IPK + 5 bytes of day values))
 const COMMAND_BODY_MAX_SIZE = 87;
 
+const examples: TCommandExampleList = [
+    {
+        name: 'absolute current values from channel #3',
+        parameters: {
+            channelList: [
+                {
+                    pulseCoefficient: 100,
+                    index: 3,
+                    value: 342
+                }
+            ]
+        },
+        hex: {
+            header: '1f 0f 04',
+            body: '08 64 d6 02'
+        }
+    }
+];
 
+
+/**
+ * Uplink command.
+ *
+ * @example create command instance from command body hex dump
+ * ```js
+ * import ExAbsCurrentMC from 'jooby-codec/commands/uplink/ExAbsCurrentMC';
+ *
+ * const commandBody = new Uint8Array([
+ *     0x1f, 0x0f, 0x04, 0x08, 0x64, 0xd6, 0x02
+ * ]);
+ * const command = ExAbsCurrentMC.fromBytes(commandBody);
+ *
+ * console.log(command.parameters);
+ * // output:
+ * {
+ *     channelList: [
+ *         {
+ *             pulseCoefficient: 100,
+ *             index: 3,
+ *             value: 342
+ *         }
+ *     ]
+ * }
+ * ```
+ *
+ * [Command format documentation](https://github.com/jooby-dev/jooby-docs/blob/main/docs/commands/uplink/ExAbsCurrentMC.md)
+ */
 class ExAbsCurrentMC extends Command {
     constructor ( public parameters: IExAbsCurrentMCParameters ) {
         super();
@@ -26,6 +71,8 @@ class ExAbsCurrentMC extends Command {
     static id = COMMAND_ID;
 
     static readonly directionType = UPLINK;
+
+    static readonly examples = examples;
 
     static readonly hasParameters = true;
 
