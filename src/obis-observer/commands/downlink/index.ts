604a3cee0cac43df1e11b2069ecbed9ceaccc6f6
DeadByElpy <deadbyelpy@gmail.com>
2023-05-16 14:32:36 +0300

    add ReadArchive command (#115)

--- src/obis-observer/commands/downlink/index.ts
+++ src/obis-observer/commands/downlink/index.ts
@@ -9,6 +9,7 @@ export {default as GetArchiveProfile} from './GetArchiveProfile.js';
 export {default as GetShortName} from './GetShortName.js';
 export {default as GetShortNameInfo} from './GetShortNameInfo.js';
 export {default as GetShortNameProfile} from './GetShortNameProfile.js';
+export {default as ReadArchive} from './ReadArchive.js';
 export {default as RemoveShortNameProfile} from './RemoveShortNameProfile.js';
 export {default as SetArchiveProfile} from './SetArchiveProfile.js';
 export {default as SetSerialPort} from './SetSerialPort.js';
