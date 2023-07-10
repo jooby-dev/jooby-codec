22a7cf6c8fe0f8fa4cd6acddec9102d2e98bc715
DarkPark <darkpark.main@gmail.com>
2023-07-10 17:55:45 +0300

    switch to @mxssfd/typedoc-theme (#164)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
+/**
+ * General tools and utils.
+ *
+ * @packageDocumentation
+ */
+
 export {default as getBytesFromHex} from './getBytesFromHex.js';
 export {default as getHexFromBytes} from './getHexFromBytes.js';
 export {default as getHexFromNumber} from './getHexFromNumber.js';
