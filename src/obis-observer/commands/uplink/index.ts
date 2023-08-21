2b7de66ceb4de996c538d5a3bc2179cda239e4fd
p.poukh <p.poukh@infomir.com>
2023-08-21 12:38:27 +0300

    obis-observer. rename files *shortName* to *obisId*

--- src/obis-observer/commands/uplink/index.ts
+++ src/obis-observer/commands/uplink/index.ts
@@ -4,28 +4,28 @@
  * @packageDocumentation
  */
 
-export {default as AddShortNameProfileResponse} from './AddShortNameProfileResponse.js';
+export {default as AddShortNameProfileResponse} from './AddObisProfileResponse.js';
 export {default as GetArchiveProfileResponse} from './GetArchiveProfileResponse.js';
 export {default as GetArchiveStateResponse} from './GetArchiveStateResponse.js';
-export {default as GetContentByObisResponse} from './GetContentByObisResponse.js';
-export {default as GetContentByShortNameResponse} from './GetContentByShortNameResponse.js';
-export {default as GetContentByShortNameStringResponse} from './GetContentByShortNameStringResponse.js';
+export {default as GetContentByObisResponse} from './GetObisContentResponse.js';
+export {default as GetContentByShortNameResponse} from './GetObisContentByIdResponse.js';
+export {default as GetContentByShortNameStringResponse} from './GetObisContentByIdStringResponse.js';
 export {default as GetDateResponse} from './GetDateResponse.js';
 export {default as GetDeviceInfoResponse} from './GetDeviceInfoResponse.js';
 export {default as GetLorawanInfoResponse} from './GetLorawanInfoResponse.js';
 export {default as GetLorawanStateResponse} from './GetLorawanStateResponse.js';
 export {default as GetReadoutStateResponse} from './GetReadoutStateResponse.js';
-export {default as GetShortNameInfoResponse} from './GetShortNameInfoResponse.js';
-export {default as GetShortNameProfileResponse} from './GetShortNameProfileResponse.js';
-export {default as GetShortNamesResponse} from './GetShortNamesResponse.js';
+export {default as GetShortNameInfoResponse} from './GetObisInfoResponse.js';
+export {default as GetShortNameProfileResponse} from './GetObisProfileResponse.js';
+export {default as GetShortNamesResponse} from './GetObisIdsResponse.js';
 export {default as ObservationReport} from './ObservationReport.js';
 export {default as ObservationReportString} from './ObservationReportString.js';
 export {default as ReadArchiveResponse} from './ReadArchiveResponse.js';
 export {default as RebootResponse} from './RebootResponse.js';
-export {default as RemoveShortNameProfileResponse} from './RemoveShortNameProfileResponse.js';
+export {default as RemoveShortNameProfileResponse} from './RemoveObisProfileResponse.js';
 export {default as SetArchiveProfileResponse} from './SetArchiveProfileResponse.js';
 export {default as SetSerialPortResponse} from './SetSerialPortResponse.js';
-export {default as SetShortNameResponse} from './SetShortNameResponse.js';
+export {default as SetShortNameResponse} from './SetObisIdResponse.js';
 export {default as UpdateImageVerifyResponse} from './UpdateImageVerifyResponse.js';
 export {default as UpdateImageWriteResponse} from './UpdateImageWriteResponse.js';
 export {default as UpdateRunResponse} from './UpdateRunResponse.js';
