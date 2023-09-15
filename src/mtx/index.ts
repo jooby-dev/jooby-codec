7c5fbb7d3e9443b257fa6c55b0e6358b601f3a01
Stanislav Kalashnik <darkpark.main@gmail.com>
2023-09-15 17:48:37 +0300

    bump version and build docs

--- src/mtx/index.ts
+++ src/mtx/index.ts
@@ -1,3 +1,4 @@
 export * as commands from './commands/index.js';
 export * as message from './message.js';
 export * as constants from './constants/index.js';
+export * as utils from './utils/index.js';
