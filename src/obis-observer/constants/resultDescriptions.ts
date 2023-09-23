8762c0cc0f979e739b66cff2c10ec878b19b26a1
p.poukh <p.poukh@infomir.com>
2023-09-23 20:01:47 +0300

    obis observer. remove obsolete result code

--- src/obis-observer/constants/resultDescriptions.ts
+++ src/obis-observer/constants/resultDescriptions.ts
@@ -11,7 +11,6 @@ export default {
     [result.METER_NOT_FOUND]: 'The meter not found',
     [result.METER_PROFILE_ALLOCATION_FAILED]: 'Meter profile allocation failed',
     [result.METER_PROFILE_NOT_FOUND]: 'The meter profile not found',
-    [result.METER_PROFILE_EXISTS]: 'The meter profile already exists',
     [result.SINGLE_MODE]: 'The single/multi meter mode collision',
     [result.MULTI_MODE_UNSUPPORTED]: 'Multi meter mode are unsupported'
 };
