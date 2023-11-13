bc96877dd2dfa7a4b89bbbb6f6662786eef5611b
DeadByElpy <deadbyelpy@gmail.com>
2023-11-13 11:27:16 +0200

    analog: add GetTime2000 (#230)

---
+++ src/analog/commands/downlink/GetTime2000.ts
@@ -0,0 +1,59 @@
+import Command, {TCommandExampleList} from '../../Command.js';
+import {DOWNLINK} from '../../../constants/directions.js';
+
+
+const COMMAND_ID = 0x09;
+
+const examples: TCommandExampleList = [
+    {
+        name: 'simple request',
+        hex: {header: '09 00', body: ''}
+    }
+];
+
+
+/**
+ * Downlink command.
+ *
+ * @example
+ * ```js
+ * import GetTime2000 from 'jooby-codec/analog/commands/downlink/GetTime2000.js';
+ *
+ * const command = new GetTime2000();
+ *
+ * // output command binary in hex representation
+ * console.log(command.toHex());
+ * // 09 00
+ * ```
+ *
+ * [Command format documentation](https://github.com/jooby-dev/jooby-docs/blob/main/docs/analog/commands/GetTime2000.md#request)
+ */
+class GetTime2000 extends Command {
+    constructor () {
+        super();
+    }
+
+
+    static readonly id = COMMAND_ID;
+
+    static readonly directionType = DOWNLINK;
+
+    static readonly examples = examples;
+
+    static readonly hasParameters = false;
+
+
+    // data - only body (without header)
+    static fromBytes () {
+        return new GetTime2000();
+    }
+
+    // returns full message - header with body
+    // eslint-disable-next-line class-methods-use-this
+    toBytes (): Uint8Array {
+        return Command.toBytes(COMMAND_ID);
+    }
+}
+
+
+export default GetTime2000;
