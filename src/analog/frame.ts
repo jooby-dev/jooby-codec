8ca042d83321cca91edc74b28d701c1d880ee9db
p.poukh <p.poukh@infomir.com>
2023-12-16 15:57:34 +0200

    analog. fix fromFrames function

--- src/analog/frame.ts
+++ src/analog/frame.ts
@@ -18,18 +18,15 @@ export const toFrame = ( commands: Array<Command> ): IAnalogFrame => {
     };
 };
 
-export const fromBytes = ( data: Uint8Array, config: Message.IMessageConfig ): IAnalogFrame => {
+export const fromBytes = ( data: Uint8Array, config?: Message.IMessageConfig ): IAnalogFrame => {
     const frame = Frame.fromBytes(data);
     const message = Message.fromBytes(frame.content, config);
 
     return {frame, message};
 };
 
-export const fromFrames = ( frames: Array<Frame.IFrame>, config: Message.IMessageConfig ): Array<IAnalogFrame> => frames.reduce(
-    (accumulator, {buffer}) => {
-        const analogFrame = fromBytes(buffer, config);
+export const fromFrames = ( frames: Array<Frame.IFrame>, config: Message.IMessageConfig ): Array<IAnalogFrame> => frames.map(frame => {
+    const message = Message.fromBytes(frame.content, config);
 
-        return [...accumulator, analogFrame];
-    },
-    [] as Array<IAnalogFrame>
-);
+    return {frame, message};
+});
