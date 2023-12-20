80aa52c9fb028ac2dc244af97ad64b881482522d
Pnom <poukh.pavel@gmail.com>
2023-12-20 12:14:32 +0200

    analog. fix names for the export namespaces frame and message (#264)

--- src/analog/frame.ts
+++ src/analog/frame.ts
@@ -1,13 +1,13 @@
 import Command from './Command.js';
-import * as Frame from '../utils/frame.js';
-import * as Message from './message.js';
+import * as baseFrame from '../utils/frame.js';
+import * as analogMessage from './message.js';
 import getBytesFromBase64 from '../utils/getBytesFromBase64.js';
 import getBytesFromHex from '../utils/getBytesFromHex.js';
 
 
 interface IAnalogFrame {
-    frame: Frame.IFrame,
-    message: Message.IMessage
+    frame: baseFrame.IFrame,
+    message: analogMessage.IMessage
 }
 
 
@@ -16,35 +16,35 @@ export const getCommands = ( {frame, message}: IAnalogFrame, isStrict: boolean =
         return [];
     }
 
-    return Message.getCommands(message, isStrict);
+    return analogMessage.getCommands(message, isStrict);
 };
 
 export const toFrame = ( commands: Array<Command> ): IAnalogFrame => {
-    const message = Message.toMessage(commands);
+    const message = analogMessage.toMessage(commands);
 
     return {
         message,
-        frame: Frame.toFrame(message.bytes)
+        frame: baseFrame.toFrame(message.bytes)
     };
 };
 
-export const fromBytes = ( bytes: Uint8Array, config?: Message.IMessageConfig ): IAnalogFrame => {
-    const frame = Frame.fromBytes(bytes);
-    const message = Message.fromBytes(frame.content, config);
+export const fromBytes = ( bytes: Uint8Array, config?: analogMessage.IMessageConfig ): IAnalogFrame => {
+    const frame = baseFrame.fromBytes(bytes);
+    const message = analogMessage.fromBytes(frame.content, config);
 
     return {frame, message};
 };
 
-export const fromHex = ( hex: string, config?: Message.IMessageConfig ) => (
+export const fromHex = ( hex: string, config?: analogMessage.IMessageConfig ) => (
     fromBytes(getBytesFromHex(hex), config)
 );
 
-export const fromBase64 = ( base64: string, config?: Message.IMessageConfig ) => (
+export const fromBase64 = ( base64: string, config?: analogMessage.IMessageConfig ) => (
     fromBytes(getBytesFromBase64(base64), config)
 );
 
-export const fromFrames = ( frames: Array<Frame.IFrame>, config?: Message.IMessageConfig ): Array<IAnalogFrame> => frames.map(frame => {
-    const message = Message.fromBytes(frame.content, config);
+export const fromFrames = ( frames: Array<baseFrame.IFrame>, config?: analogMessage.IMessageConfig ): Array<IAnalogFrame> => frames.map(frame => {
+    const message = analogMessage.fromBytes(frame.content, config);
 
     return {frame, message};
 });
