a809e425358e1b25ad00f3895c0b74aa499443fc
p.poukh <p.poukh@infomir.com>
2023-12-20 12:09:32 +0200

    analog. fix names for the export namespaces frame and message

--- src/analog/index.ts
+++ src/analog/index.ts
@@ -1,4 +1,4 @@
 export * as commands from './commands/index.js';
-export * as Message from './message.js';
-export * as Frame from './frame.js';
+export * as message from './message.js';
+export * as frame from './frame.js';
 export * as constants from './constants/index.js';
