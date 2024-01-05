6dbb822c00d71b6355ccaad1b76da38eb914030c
Pnom <poukh.pavel@gmail.com>
2024-01-05 15:23:19 +0200

    MtxLora. support MtxLora codec
    * MtxLora. support MtxLora codec
    
    * mtxLora. review results

---
+++ src/mtxLora/commands/uplink/MtxCommand.ts
@@ -0,0 +1,30 @@
+import MtxCommandBase from '../MtxCommandBase.js';
+import {UPLINK} from '../../../constants/directions.js';
+
+
+/**
+ * Uplink command.
+ *
+ * @example
+ * ```js
+ * import MtxCommand from 'jooby-codec/mtxLora/commands/uplink/MtxCommand.js';
+ *
+ * const commandBody = new Uint8Array([
+ *     0x1e, 0x07, 0x02, 0x53, 0x00, 0x01, 0x02, 0x03, 0x04
+ * ]);
+ * const command = MtxCommand.fromBytes(commandBody);
+ *
+ * console.log(command.parameters);
+ * // output:
+ * {
+ *     sequence: 2,
+ *     last: false,
+ *     fragmentsNumber: 5,
+ *     fragmentIndex: 3,
+ *     data: Uint8Array(3) [0, 1, 2, 3, 4, buffer: ...
+ * }
+ * ```
+ */
+export default class MtxCommand extends MtxCommandBase {
+    static readonly directionType = UPLINK;
+}
