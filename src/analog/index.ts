f767eefce70718eb63bf70550280a1c25b843ef7
p.poukh <p.poukh@infomir.com>
2024-01-15 11:34:18 +0200

    add UnknownCommand to main codecs index

--- src/analog/index.ts
+++ src/analog/index.ts
@@ -3,3 +3,4 @@ export * as message from './message.js';
 export * as frame from './frame.js';
 export * as constants from './constants/index.js';
 export {default as MtxCommandsCollector} from './MtxCommandsCollector.js';
+export {default as UnknownCommand} from './UnknownCommand.js';
