4c66dc01458c4eb2dcac44790f7ab4d63c8af092
p.poukh <p.poukh@infomir.com>
2024-01-30 17:00:50 +0200

    mtxLora. simplify message test

--- tests/mtxLora/message.test.ts
+++ tests/mtxLora/message.test.ts
@@ -13,9 +13,8 @@ import {DOWNLINK, UPLINK} from '../../src/constants/directions.js';
 interface IMessage {
     name: string,
     hex: string,
-    direction: number,
     messageId: number,
-    accessLevel: number,
+    config: message.IMessageConfig
     commands: Array<Command>
 }
 
@@ -26,8 +25,10 @@ const downlinkMessages: TMessageList = [
         name: 'downlink command',
         hex: '0a 10 10 69 05 2a 43 03 05 03',
         messageId: 10,
-        direction: DOWNLINK,
-        accessLevel: UNENCRYPTED,
+        config: {
+            accessLevel: UNENCRYPTED,
+            direction: DOWNLINK
+        },
         commands: [
             new downlinkCommands.GetHalfhoursEnergies({
                 date: {
@@ -55,8 +56,10 @@ const uplinkMessages: TMessageList = [
         name: 'uplink commands',
         hex: '10 10 10 78 0c 2a 43 11 11 00 00 10 00 00 00 20 00 69 0d 2a 43 01 01 02 00 00 10 00 00 00 20 00',
         messageId: 16,
-        direction: UPLINK,
-        accessLevel: UNENCRYPTED,
+        config: {
+            accessLevel: UNENCRYPTED,
+            direction: UPLINK
+        },
         commands: [
             new uplinkCommands.GetDayEnergies({
                 date: {
@@ -88,13 +91,13 @@ const uplinkMessages: TMessageList = [
 
 
 const checkMessage = ( messageParams: IMessage ) => {
-    const {hex, messageId, accessLevel, direction, commands} = messageParams;
-    const messageBytes = message.toBytes(messageId, accessLevel, commands);
-    const messageData = message.fromHex(hex, {direction});
+    const {hex, messageId, config, commands} = messageParams;
+    const messageBytes = message.toBytes(messageId, commands, config);
+    const messageData = message.fromHex(hex, config);
 
     expect(getHexFromBytes(messageBytes)).toEqual(hex);
     expect(messageData.messageId).toEqual(messageId);
-    expect(messageData.accessLevel).toEqual(accessLevel);
+    expect(messageData.accessLevel).toEqual(config.accessLevel);
     expect(messageData.commands?.map(item => item.command)).toStrictEqual(commands);
 };
 
