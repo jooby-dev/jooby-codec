02420918a57c8650df066fee85d874ece12131f0
p.poukh <p.poukh@infomir.com>
2024-03-04 18:25:48 +0200

    mtxLora. fix GetHalfhoursEnergies command code

--- tests/mtxLora/message.test.ts
+++ tests/mtxLora/message.test.ts
@@ -23,7 +23,7 @@ type TMessageList = Array<IMessage>;
 const downlinkMessages: TMessageList = [
     {
         name: 'downlink command',
-        hex: '0a 10 10 69 05 2a 43 03 05 03',
+        hex: '0a 10 10 6f 05 2a 43 03 05 03',
         messageId: 10,
         config: {
             accessLevel: UNENCRYPTED,
@@ -54,7 +54,7 @@ const downlinkMessages: TMessageList = [
 const uplinkMessages: TMessageList = [
     {
         name: 'uplink commands',
-        hex: '10 10 10 78 0c 2a 43 11 11 00 00 10 00 00 00 20 00 69 0d 2a 43 01 01 02 00 00 10 00 00 00 20 00',
+        hex: '10 10 10 78 0c 2a 43 11 11 00 00 10 00 00 00 20 00 6f 0d 2a 43 01 01 02 00 00 10 00 00 00 20 00',
         messageId: 16,
         config: {
             accessLevel: UNENCRYPTED,
