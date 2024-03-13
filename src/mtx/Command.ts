ae3981e4ada5f515ed354a86bf5ff35fe582409b
p.poukh <p.poukh@infomir.com>
2024-03-13 12:05:09 +0200

    mtx: move IDlmsJsonOptions to mtx from mtxLora

--- src/mtx/Command.ts
+++ src/mtx/Command.ts
@@ -1,6 +1,15 @@
-import Command from '../Command.js';
+import Command, {TJsonOptions, defaultJsonOptions} from '../Command.js';
 
 export default Command;
 export {ICommandExample, TCommandExampleList, TJsonOptions, defaultJsonOptions} from '../Command.js';
 
 export const COMMAND_HEADER_SIZE = 2;
+
+export interface IDlmsJsonOptions extends TJsonOptions {
+    dlms: boolean
+}
+
+export const defaultDlmsJsonOptions = {
+    ...defaultJsonOptions,
+    dlms: false
+};
--- src/mtxLora/Command.ts
+++ src/mtxLora/Command.ts
@@ -1,13 +1,4 @@
-import Command, {TJsonOptions, defaultJsonOptions} from '../mtx/Command.js';
+import Command from '../mtx/Command.js';
 
 export default Command;
 export * from '../mtx/Command.js';
-
-export interface IDlmsJsonOptions extends TJsonOptions {
-    dlms: boolean
-}
-
-export const defaultDlmsJsonOptions = {
-    ...defaultJsonOptions,
-    dlms: false
-};
