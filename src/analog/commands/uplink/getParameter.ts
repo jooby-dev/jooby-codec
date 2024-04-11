5ccf83ed0491a37122069816e7c50cd5e92d311e
DeadByElpy <deadbyelpy@gmail.com>
2024-04-11 18:01:25 +0300

    analog: add device parameters en/decoding

--- src/analog/commands/uplink/getParameter.ts
+++ src/analog/commands/uplink/getParameter.ts
@@ -45,59 +45,59 @@ export const examples: command.TCommandExamples = {
             0x04, 0x05,
             0x01, 0x00, 0x00, 0x00, 0x04
         ]
+    },
+    'absolute data (not multichannel device)': {
+        id,
+        name,
+        headerSize,
+        parameters: {
+            id: 23,
+            data: {meterValue: 204, pulseCoefficient: 100, value: 2023}
+        },
+        bytes: [
+            0x04, 0x0a,
+            0x17, 0x00, 0x00, 0x00, 0xcc, 0x83, 0x00, 0x00, 0x07, 0xe7
+        ]
+    },
+    'state of absolute data (not multichannel device)': {
+        id,
+        name,
+        headerSize,
+        parameters: {
+            id: 24,
+            data: {state: 1}
+        },
+        bytes: [
+            0x04, 0x02,
+            0x18, 0x01
+        ]
+    },
+    'absolute data for multichannel device (1 channel)': {
+        id,
+        name,
+        headerSize,
+        parameters: {
+            id: 29,
+            data: {channel: 1, meterValue: 402, pulseCoefficient: 1000, value: 2032}
+        },
+        bytes: [
+            0x04, 0x0b,
+            0x1d, 0x00, 0x00, 0x00, 0x01, 0x92, 0x84, 0x00, 0x00, 0x07, 0xf0
+        ]
+    },
+    'state of absolute data for multichannel device (1 channel)': {
+        id,
+        name,
+        headerSize,
+        parameters: {
+            id: 30,
+            data: {channel: 2, state: 1}
+        },
+        bytes: [
+            0x04, 0x03,
+            0x1e, 0x01, 0x01
+        ]
     }
-    // 'absolute data (not multichannel device)': {
-    //     id,
-    //     name,
-    //     headerSize,
-    //     parameters: {
-    //         id: 23,
-    //         data: null
-    //     },
-    //     bytes: [
-    //         0x04, 0x01,
-    //         0x17
-    //     ]
-    // },
-    // 'state of absolute data (not multichannel device)': {
-    //     id,
-    //     name,
-    //     headerSize,
-    //     parameters: {
-    //         id: 24,
-    //         data: null
-    //     },
-    //     bytes: [
-    //         0x04, 0x01,
-    //         0x18
-    //     ]
-    // },
-    // 'state of absolute for multichannel device (1 channel)': {
-    //     id,
-    //     name,
-    //     headerSize,
-    //     parameters: {
-    //         id: 29,
-    //         data: {channel: 1}
-    //     },
-    //     bytes: [
-    //         0x04, 0x02,
-    //         0x1d, 0x00
-    //     ]
-    // },
-    // 'state of absolute data for multichannel device (1 channel)': {
-    //     id,
-    //     name,
-    //     headerSize,
-    //     parameters: {
-    //         id: 30,
-    //         data: {channel: 1}
-    //     },
-    //     bytes: [
-    //         0x04, 0x02,
-    //         0x1e, 0x00
-    //     ]
-    // }
 };
 
 
