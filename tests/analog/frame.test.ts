ed461bede4d830f6af6da57d10681bf335b1146f
DeadByElpy <deadbyelpy@gmail.com>
2024-04-18 18:10:51 +0300

    rework frame tests

--- tests/analog/frame.test.ts
+++ tests/analog/frame.test.ts
@@ -1,15 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as frameUtils from '../../src/analog/utils/frame.js';
-import * as messageImplementations from '../../src/analog/message/index.js';
+import * as downlinkMessage from '../../src/analog/message/downlink.js';
 import * as downlinkCommands from '../../src/analog/commands/downlink/index.js';
+import {IInvalidMessage, IMessage} from '../../src/analog/message/types.js';
+import {ICommand} from '../../src/analog/utils/command.js';
 
 
 const command1 = {
+    bytes: [0x02, 0x05, 0x4e, 0x2b, 0xbd, 0x98, 0xad],
+    headerSize: 2,
     id: downlinkCommands.setTime2000.id,
+    name: 'setTime2000',
     parameters: {sequenceNumber: 78, seconds: 733845677}
 };
 const command2 = {
-    id: downlinkCommands.setParameter.id,
+    bytes: [0x03, 0x07, 0x0a, 0x00, 0x64, 0x0c, 0x96, 0x00, 0xe9],
+    id: 0x03,
+    headerSize: 2,
+    name: 'setParameter',
     parameters: {
         id: 10,
         data: {
@@ -20,77 +27,77 @@ const command2 = {
     }
 };
 const command3 = {
-    id: downlinkCommands.getStatus.id,
+    bytes: [0x14, 0x00],
+    id: 0x14,
+    headerSize: 2,
+    name: 'getStatus',
     parameters: {}
 };
 
 
 describe('frame validation', () => {
     test('test valid frame', () => {
-        const {frame/* , message */} = frameUtils.toFrame(messageImplementations.downlink, [command1, command2]);
+        const {frame, message: anyMessage} = frameUtils.toFrame(downlinkMessage, [command1, command2]);
+        const message = anyMessage as IInvalidMessage;
+
         expect(frame.isValid).toBe(true);
-        // expect(message.error).toBe(undefined);
+        expect(message.error).toBe(undefined);
     });
 
     test('test invalid frame', () => {
         const hex = '7e 02 05 4e 2b bd 98 ab 03 07 0a 00 64 0c 96 00 e9 a6 8a 94 7e';
-        const {frame/* , message */} = frameUtils.fromHex(messageImplementations.downlink, hex);
+        const {frame, message: anyMessage} = frameUtils.fromHex(downlinkMessage, hex);
+        const message = anyMessage as IInvalidMessage;
 
         expect(frame.isValid).toBe(false);
-        // expect(message.error).toBe(false);
+        expect(message.error).toBe('mismatch LRC');
     });
 });
 
-// describe('getCommands', () => {
-//     test('test valid input', () => {
-//         const hex = '7e 02 05 4e 2b bd 98 ad 03 07 0a 00 64 0c 96 00 e9 a6 8a 94 7e';
-//         const frame = frame.fromHex(hex);
-//         const strictResult = Frame.getCommands(frame, true);
+describe('getCommands', () => {
+    test('test valid input', () => {
+        const hex = '7e 02 05 4e 2b bd 98 ad 03 07 0a 00 64 0c 96 00 e9 a6 8a 94 7e';
+        const {frame, message: anyMessage} = frameUtils.fromHex(downlinkMessage, hex);
+        // it must be valid, but need to access to 'error' property which exists only in invalid message
+        const invalidMessage = anyMessage as IInvalidMessage;
+        const message = anyMessage as IMessage;
+        const setTime2000Command = message.commands[0] as ICommand;
+        const setParameterCommand = message.commands[1] as ICommand;
 
-//         expect(frame.frame.isValid).toBe(true);
-//         expect(frame.message.isValid).toBe(true);
-//         expect(strictResult.length).toBe(2);
-//         expect(strictResult[0]).toBeInstanceOf(downlinkCommands.SetTime2000);
-//         expect(strictResult[1]).toBeInstanceOf(downlinkCommands.SetParameter);
+        expect(frame.isValid).toBe(true);
+        expect(invalidMessage.error).toBe(undefined);
+        expect(message.commands.length).toBe(2);
+        expect(setTime2000Command.id).toEqual(downlinkCommands.setTime2000.id);
+        expect(setParameterCommand.id).toEqual(downlinkCommands.setParameter.id);
+    });
 
-//         const nonStrictResult = Frame.getCommands(frame, false);
+    test('test invalid input', () => {
+        const hex = '7e 02 05 4e 2b bd 98 ab 03 07 0a 00 64 0c 96 00 e9 a6 8a 94 7e';
+        const {frame, message: anyMessage} = frameUtils.fromHex(downlinkMessage, hex);
+        const invalidMessage = anyMessage as IInvalidMessage;
+        const message = anyMessage as IMessage;
 
-//         expect(nonStrictResult.length).toBe(2);
-//         expect(nonStrictResult[0]).toBeInstanceOf(downlinkCommands.SetTime2000);
-//         expect(nonStrictResult[1]).toBeInstanceOf(downlinkCommands.SetParameter);
-
-//         expect(strictResult).toStrictEqual(strictResult);
-//     });
-
-//     test('test invalid input', () => {
-//         const hex = '7e 02 05 4e 2b bd 98 ab 03 07 0a 00 64 0c 96 00 e9 a6 8a 94 7e';
-//         const frame = Frame.fromHex(hex);
-//         const strictResult = Frame.getCommands(frame, true);
-
-//         expect(frame.frame.isValid).toBe(false);
-//         expect(frame.message.isValid).toBe(false);
-//         expect(strictResult.length).toBe(0);
-
-//         const nonStrictResult = Frame.getCommands(frame, false);
-
-//         expect(nonStrictResult.length).toBe(2);
-//         expect(nonStrictResult[0]).toBeInstanceOf(downlinkCommands.SetTime2000);
-//         expect(nonStrictResult[1]).toBeInstanceOf(downlinkCommands.SetParameter);
-//     });
-// });
+        expect(frame.isValid).toBe(false);
+        expect(invalidMessage.error).toBe('mismatch LRC');
+        expect(message.commands).toBe(undefined);
+    });
+});
 
 describe('test fromFrames', () => {
     test('test 1', () => {
-        const frame1 = frameUtils.toFrame(messageImplementations.downlink, [command1, command2]);
-        const frame2 = frameUtils.toFrame(messageImplementations.downlink, [command3]);
-        const result = frameUtils.fromFrames(messageImplementations.downlink, [frame1.frame, frame2.frame]);
-        // message unknown
-        // const commands = result.flatMap(({message}) => message.commands);
+        const frame1 = frameUtils.toFrame(downlinkMessage, [command1, command2]);
+        const frame2 = frameUtils.toFrame(downlinkMessage, [command3]);
+        const result = frameUtils.fromFrames(downlinkMessage, [frame1.frame, frame2.frame]);
+        const commands = result.flatMap(({message: anyMessage}) => {
+            const message = anyMessage as IMessage;
+
+            return message.commands;
+        });
 
         expect(result.length).toBe(2);
-        // expect(commands.length).toBe(3);
-        // expect(commands[0]).toStrictEqual(command1);
-        // expect(commands[1]).toStrictEqual(command2);
-        // expect(commands[2]).toStrictEqual(command3);
+        expect(commands.length).toBe(3);
+        expect(commands[0]).toStrictEqual(command1);
+        expect(commands[1]).toStrictEqual(command2);
+        expect(commands[2]).toStrictEqual(command3);
     });
 });
