6aeda593b4b56cc8108e2399569ab32c64ed5576
DeadByElpy <deadbyelpy@gmail.com>
2024-04-29 12:35:33 +0300

    remove unused constants types

--- src/mtx/constants/types.ts
+++
@@ -1,7 +0,0 @@
-import * as types from '../../types.js';
-
-/**
- * Unique for a single direction (uplink/downlink) command name.
- * It is used in message build.
- */
-export type TAccessLevel = types.BrandType<number, '0 | 1 | 2 | 3'>;
