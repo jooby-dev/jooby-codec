b853f665c572d775f0a4279dc1980a8b3c9dfde1
Stanislav Kalashnik <darkpark.main@gmail.com>
2024-05-29 18:33:51 +0300

    add getMonthDemand command

---
+++ src/mtx/commands/uplink/getMonthDemand.ts
@@ -0,0 +1,100 @@
+/**
+ * Uplink command to get energy A+ for 4 tariffs (T1-T4) for a given month.
+ *
+ * @packageDocumentation
+ *
+ * @example create command instance from command body hex dump
+ * ```js
+ * import * as getMonthDemand from 'jooby-codec/mtx/commands/uplink/getMonthDemand.js';
+ *
+ * // received energies
+ * const bytes = [0x18, 0x03, 0x02, 0x66, 0xf2, 0xae, 0x00, 0x32, 0xe0, 0x64, 0x00, 0x00, 0x09, 0x1d, 0x00, 0x20, 0xbd, 0x57];
+ *
+ * // decoded payload
+ * const parameters = getMonthDemand.fromBytes(bytes);
+ *
+ * console.log(parameters);
+ * // output:
+ * {
+ *     year: 24,
+ *     month: 3,
+ *     energies: [40301230, 3334244, 2333, 2145623]
+ * }
+ * ```
+ *
+ * [Command format documentation](https://github.com/jooby-dev/jooby-docs/blob/main/docs/mtx/commands/uplink/GetMonthDemand.md#response)
+ */
+
+import * as types from '../../types.js';
+import * as command from '../../utils/command.js';
+import {READ_ONLY} from '../../constants/accessLevels.js';
+import CommandBinaryBuffer, {ICommandBinaryBuffer, IEnergies} from '../../utils/CommandBinaryBuffer.js';
+
+
+interface IGetMonthDemandResponseParameters {
+    year: types.TYear2000,
+    month: types.TMonth,
+    energies: IEnergies
+}
+
+
+export const id: types.TCommandId = 0x17;
+export const name: types.TCommandName = 'getMonthDemand';
+export const headerSize = 2;
+export const accessLevel: types.TAccessLevel = READ_ONLY;
+export const maxSize = 18;
+export const isLoraOnly = false;
+
+export const examples: command.TCommandExamples = {
+    'response energy for 2024.03': {
+        id,
+        name,
+        headerSize,
+        maxSize,
+        accessLevel,
+        parameters: {
+            year: 24,
+            month: 3,
+            energies: [40301230, 3334244, 2333, 2145623]
+        },
+        bytes: [
+            0x17, 0x12,
+            0x18, 0x03, 0x02, 0x66, 0xf2, 0xae, 0x00, 0x32, 0xe0, 0x64, 0x00, 0x00, 0x09, 0x1d, 0x00, 0x20, 0xbd, 0x57
+        ]
+    }
+};
+
+
+/**
+ * Decode command parameters.
+ *
+ * @param bytes - only body (without header)
+ * @returns command payload
+ */
+export const fromBytes = ( bytes: types.TBytes ): IGetMonthDemandResponseParameters => {
+    const buffer: ICommandBinaryBuffer = new CommandBinaryBuffer(bytes);
+
+    return {
+        year: buffer.getUint8() as unknown as types.TYear2000,
+        month: buffer.getUint8() as unknown as types.TMonth,
+        energies: buffer.getEnergies()
+    };
+};
+
+
+/**
+ * Encode command parameters.
+ *
+ * @param parameters - command payload
+ * @returns full message (header with body)
+ */
+export const toBytes = ( parameters: IGetMonthDemandResponseParameters ): types.TBytes => {
+    const buffer: ICommandBinaryBuffer = new CommandBinaryBuffer(maxSize);
+
+    // body
+    buffer.setUint8(parameters.year as unknown as types.TUint8);
+    buffer.setUint8(parameters.month as unknown as types.TUint8);
+    buffer.setEnergies(parameters.energies);
+
+    return command.toBytes(id, buffer.data);
+};
