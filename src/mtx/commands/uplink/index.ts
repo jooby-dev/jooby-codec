1ba4d2b8a50697b71fea7374fd901de111ce7ca7
Stanislav Kalashnik <darkpark.main@gmail.com>
2024-06-06 16:29:47 +0300

    add getHalfHourDemandPrevious command

--- src/mtx/commands/uplink/index.ts
+++ src/mtx/commands/uplink/index.ts
@@ -30,6 +30,7 @@ export * as getEventStatus from './getEventStatus.js';
 export * as getExtendedCurrentValues from './getExtendedCurrentValues.js';
 export * as getExtendedCurrentValues2 from './getExtendedCurrentValues2.js';
 export * as getHalfHourDemand from './getHalfHourDemand.js';
+export * as getHalfHourDemandPrevious from './getHalfHourDemandPrevious.js';
 export * as getHalfhoursEnergies from './getHalfhoursEnergies.js';
 export * as getMonthDemand from './getMonthDemand.js';
 export * as getOperatorParametersExtended3 from './getOperatorParametersExtended3.js';
