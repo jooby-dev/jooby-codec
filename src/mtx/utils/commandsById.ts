0819fe79e5c42edba8f831158c510866bd8b39bb
p.poukh <p.poukh@infomir.com>
2024-06-12 12:01:19 +0300

    move commandsById files to commands folder

--- src/mtx/utils/commandsById.ts
+++
@@ -1,6 +0,0 @@
-import {downlink as originalDownlink, uplink as originalUplink} from '../commands/index.js';
-import getCommandsById from '../../utils/getCommandsById.js';
-
-
-export const downlink = getCommandsById(originalDownlink);
-export const uplink = getCommandsById(originalUplink);
