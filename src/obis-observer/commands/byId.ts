11874ee1d82cd6f16a11e3835399bc3f6ec70c2c
Stanislav Kalashnik <darkpark.main@gmail.com>
2024-06-13 11:51:31 +0300

    add downlinkById/uplinkById to all commands export

--- src/obis-observer/commands/byId.ts
+++
@@ -1,6 +0,0 @@
-import {downlink as originalDownlink, uplink as originalUplink} from './index.js';
-import getCommandsById from '../../utils/getCommandsById.js';
-
-
-export const downlink = getCommandsById(originalDownlink);
-export const uplink = getCommandsById(originalUplink);
