7bc5a8d8b5c008df854c633e7e628f8b03bbddce
Alex <alex.andrutskii@gmail.com>
2024-07-11 10:11:40 +0300

    mtx3: add getEnergyExportDayPrevious, getMonthDemandExport commands

--- src/mtx3/commands/downlink/index.ts
+++ src/mtx3/commands/downlink/index.ts
@@ -23,6 +23,7 @@ export * as getHalfHourDemand from '../../../mtx/commands/downlink/getHalfHourDe
 export * as getHalfhoursEnergies from '../../../mtx/commands/downlink/getHalfhoursEnergies.js';
 export * as getMagneticFieldThreshold from '../../../mtx/commands/downlink/getMagneticFieldThreshold.js';
 export * as getMonthDemand from '../../../mtx/commands/downlink/getMonthDemand.js';
+export * as getMonthDemandExport from '../../../mtx/commands/downlink/getMonthDemandExport.js';
 export * as getOperatorParameters from '../../../mtx/commands/downlink/getOperatorParameters.js';
 export * as getRatePlanInfo from '../../../mtx/commands/downlink/getRatePlanInfo.js';
 export * as getSaldo from '../../../mtx/commands/downlink/getSaldo.js';
@@ -56,6 +57,7 @@ export * as getDisplayParam from './getDisplayParam.js';
 export * as getEnergy from './getEnergy.js';
 export * as getEnergyDayPrevious from './getEnergyDayPrevious.js';
 export * as getEnergyExport from './getEnergyExport.js';
+export * as getEnergyExportDayPrevious from './getEnergyExportDayPrevious.js';
 export * as getHalfHourDemandVare from './getHalfHourDemandVare.js';
 export * as getHalfHourDemandVari from './getHalfHourDemandVari.js';
 export * as getOperatorParametersExtended from './getOperatorParametersExtended.js';
