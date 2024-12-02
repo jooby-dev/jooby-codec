69ea99a2939dc403aec99982b0e4baef4e6fd8f3
p.poukh <p.poukh@infomir.com>
2024-12-02 22:44:45 +0200

    analog, mtx1/3: review results (3)

--- src/analog/constants/index.ts
+++ src/analog/constants/index.ts
@@ -4,6 +4,11 @@
  * @packageDocumentation
  */
 
+
+export {default as channelsNames} from './channelsNames.js';
+export {default as deviceParametersNames} from './deviceParametersNames.js';
+export {default as eventsNames} from './eventsNames.js';
+export {default as rx2SpreadFactorsNames} from './rx2SpreadFactorsNames.js';
 export * as deviceParameters from './deviceParameters.js';
 export * as events from './events.js';
 export * as hardwareTypes from './hardwareTypes.js';
--- src/mtx1/constants/index.ts
+++ src/mtx1/constants/index.ts
@@ -4,14 +4,19 @@
  * @packageDocumentation
  */
 
+
+export {default as criticalEventsNames} from './criticalEventsNames.js';
+export {default as frameNames} from './frameNames.js';
 export {default as resultNames} from './resultNames.js';
 export {default as screenNames} from './screenNames.js';
+export {default as eventsNames} from './eventsNames.js';
 export * as accessLevels from './accessLevels.js';
+export * as criticalEvents from './criticalEvents.js';
+export * as energyTypes from './energyTypes.js';
+export * as events from './events.js';
 export * as frameTypes from './frameTypes.js';
 export * as meterTypes from './meterTypes.js';
 export * as resultCodes from './resultCodes.js';
 export * as screenIds from './screenIds.js';
 export * as screenSets from './screenSets.js';
-export * as criticalEvents from './criticalEvents.js';
 export * as specialOperationTypes from './specialOperationTypes.js';
-export * as energyTypes from './energyTypes.js';
