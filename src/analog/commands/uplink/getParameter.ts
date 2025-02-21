072eb5edf0c673cae6d58808e1358c6b46cf740f
Pnom <poukh.pavel@gmail.com>
2025-02-21 18:24:43 +0200

    analog: add KEEP_LORA_CONNECTION_ON_REMOVAL parameter (#605)
    * analog: add KEEP_LORA_CONNECTION_ON_REMOVAL parameter
    
    * analog: review results
    
    ---------
    
    Co-authored-by: p.poukh <p.poukh@infomir.com>

--- src/analog/commands/uplink/getParameter.ts
+++ src/analog/commands/uplink/getParameter.ts
@@ -161,6 +161,38 @@ export const examples: command.TCommandExamples = {
             0x04, 0x04,
             0x34, 0x02, 0x03, 0x14
         ]
+    },
+    'time synchronization period in seconds via MAC commands': {
+        id,
+        name,
+        headerSize,
+        parameters: {
+            id: 58,
+            name: 'TIME_SYNCHRONIZATION_PERIOD_VIA_MAC',
+            data: {
+                period: 1440
+            }
+        },
+        bytes: [
+            0x04, 0x05,
+            0x3a, 0x00, 0x00, 0x05, 0xa0
+        ]
+    },
+    'keep lora connection even after being removed': {
+        id,
+        name,
+        headerSize,
+        parameters: {
+            id: 59,
+            name: 'KEEP_LORA_CONNECTION_ON_REMOVAL',
+            data: {
+                value: true
+            }
+        },
+        bytes: [
+            0x04, 0x02,
+            0x3b, 0x01
+        ]
     }
 };
 
