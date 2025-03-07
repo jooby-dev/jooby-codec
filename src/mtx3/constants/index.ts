a639d715e2a3062187db75adbcae14c930325930
aleks-and <44006743+aleks-and@users.noreply.github.com>
2025-03-07 14:59:58 +0300

    feat(mtx3): add screenIds, screenNames, and screenSets (#613)
    * feat(mtx3): add screenIds, screenNames, and screenSets
    
    * docs(mtx1): add missing periods in screenIds comments

--- src/mtx3/constants/index.ts
+++ src/mtx3/constants/index.ts
@@ -9,9 +9,12 @@ export * from '../../mtx1/constants/index.js';
 
 export {default as downlinkNames} from './downlinkNames.js';
 export {default as eventNames} from './eventNames.js';
+export {default as screenNames} from './screenNames.js';
 export {default as uplinkNames} from './uplinkNames.js';
 export * as baudRates from './baudRates.js';
 export * as downlinkIds from './downlinkIds.js';
 export * as energyTypes from './energyTypes.js';
 export * as events from './events.js';
+export * as screenIds from './screenIds.js';
+export * as screenSets from './screenSets.js';
 export * as uplinkIds from './uplinkIds.js';
