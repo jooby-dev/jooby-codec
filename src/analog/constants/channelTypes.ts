34917c1681bc553162f7a739c8dd84075c11b28e
Sergey Beregovoy <s.beregovoy@infomir.com>
2025-05-29 21:04:13 +0300

    minor fixes

--- src/analog/constants/channelTypes.ts
+++ src/analog/constants/channelTypes.ts
@@ -1,11 +1,10 @@
 /** Channel is inactive and not assigned any function */
 export const IDLE = 0;
 
-// TODO: check description of 4PU module in jooby-docs/google docs
-/**  Pulse sensor, count pulses */
+/** Pulse sensor, count pulses */
 export const PULSE_SENSOR = 1;
 
-/**  Channel is configured to supply power to connected components */
+/** Channel is configured to supply power to connected components */
 export const POWER_CHANNEL = 2;
 
 /** Binary sensor, detecting on/off or open/closed states */
