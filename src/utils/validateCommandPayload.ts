fcf1612c9e5be07e0baac026ec1c77b82164b53c
p.poukh <p.poukh@infomir.com>
2026-02-18 12:43:12 +0200

    feat(utils): add validateRangeCommandPayload function

--- src/utils/validateCommandPayload.ts
+++ src/utils/validateCommandPayload.ts
@@ -10,7 +10,7 @@ import getHexFromBytes from './getHexFromBytes.js';
  * {fixedLength: number}
  * {minimalLength: number, maximalLength: number}
  */
-export default ( commandName: string, bytes: types.TBytes, expectedLength: number ) => {
+export const validateFixedCommandPayload = ( commandName: string, bytes: types.TBytes, expectedLength: number ) => {
     if ( !commandName ) {
         throw new Error('Command name is required.');
     }
@@ -25,3 +25,22 @@ export default ( commandName: string, bytes: types.TBytes, expectedLength: numbe
         throw new Error(`Wrong buffer size for ${commandName}: ${bytes.length}. Expected: ${expectedLength}. Payload: 0x${hex}.`);
     }
 };
+
+export const validateRangeCommandPayload = ( commandName: string, bytes: types.TBytes, range: {min?: number; max?: number} ) => {
+    if ( !commandName ) {
+        throw new Error('Command name is required.');
+    }
+
+    if ( bytes && !Array.isArray(bytes) ) {
+        throw new Error(`Invalid payload for ${commandName}. Expected array, got: ${typeof bytes}.`);
+    }
+
+    if ( (range.min > 0 && bytes.length < range.min ) || (range.max > 0 && bytes.length > range.max ) ) {
+        const hex = getHexFromBytes(bytes, {separator: ''});
+
+        throw new Error(`Wrong buffer size for ${commandName}: ${bytes.length}. Expected: ${JSON.stringify(range)}. Payload: 0x${hex}.`);
+    }
+};
+
+
+export default validateFixedCommandPayload;
