d39795641e1c8336b7005566bc55dbe70eff0291
p.poukh <p.poukh@infomir.com>
2026-02-19 14:01:42 +0200

    feat: move crc related code to own file from frame.ts

---
+++ src/utils/payloadCrc16.ts
@@ -0,0 +1,46 @@
+import {TBytes} from '../types.js';
+import BinaryBuffer, {IBinaryBuffer} from './BinaryBuffer.js';
+import calculateCrc16 from './calculateCrc16.js';
+
+
+const getBytesCrc = ( bytes: TBytes ): number | undefined => {
+    if ( bytes.length >= 2 ) {
+        const crcBuffer: IBinaryBuffer = new BinaryBuffer(bytes.slice(-2));
+
+        return crcBuffer.getUint16();
+    }
+
+    return undefined;
+};
+
+const convertCrcToBytes = ( crc: number ) : TBytes => {
+    const buffer: IBinaryBuffer = new BinaryBuffer(2);
+
+    buffer.setUint16(crc);
+
+    return buffer.toUint8Array();
+};
+
+
+export const parse = ( bytes: TBytes ) => {
+    const receivedCrc = getBytesCrc(bytes);
+    const payload = bytes.slice(0, bytes.length - 2);
+    const calculatedCrc = calculateCrc16(payload);
+
+    return {
+        payload,
+        crc: {
+            calculated: calculatedCrc,
+            received: receivedCrc
+        }
+    };
+};
+
+export const appendCrc = ( payload: TBytes ) => {
+    if ( payload.length === 0 ) return [];
+
+    const crc = calculateCrc16(payload);
+    const crcBytes = convertCrcToBytes(crc);
+
+    return [...payload, ...crcBytes];
+};
