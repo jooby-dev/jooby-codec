e35fb9ec43a2a3b73a67c4ebfcefa5736273d441
p.poukh <p.poukh@infomir.com>
2026-02-25 19:17:45 +0200

    feat: support gsm parameters

--- src/mtx1/utils/CommandBinaryBuffer.ts
+++ src/mtx1/utils/CommandBinaryBuffer.ts
@@ -19,6 +19,8 @@ import * as frameTypes from '../constants/frameTypes.js';
 import frameNames from '../constants/frameNames.js';
 import {baudRates, events, eventNames} from '../constants/index.js';
 import * as demandTypes from '../constants/demandTypes.js';
+import * as gsmAccessTypes from '../constants/gsmAccessTypes.js';
+import * as gsmBlockTypes from '../constants/gsmBlockTypes.js';
 
 
 export const frameHeaderSize = 5;
@@ -55,6 +57,224 @@ export interface IGsmBlock {
     index: types.TUint8,
     data: types.TBytes
 }
+
+export interface IGsmConfiguration {
+    blockVersion: types.TUint8,
+
+    configurationId: types.TUint32,
+
+    /**
+     * possible value is one of {@link gsmAccessTypes}
+     */
+    gsmAccessTypes: types.TUint8,
+
+    /**
+     * GPRS access point.
+     */
+    gprsAccessPoint: string,
+
+    /**
+     * GPRS user name.
+     */
+    gprsUserName: string,
+
+    /**
+     * GPRS password.
+     */
+    gprsPassword: string,
+
+    /**
+     * TCP server port for the main data channel.
+     * The device listens on this port and accepts incoming connections
+     * to receive commands / messages.
+     */
+    commandServerPort: types.TUint16;
+
+    /**
+     * TCP server port used for activity monitoring.
+     * External systems can connect to this port to check whether
+     * the device is online and responsive.
+     */
+    activityServerPort: types.TUint16;
+
+
+    /**
+     * Period between activity pings in seconds.
+     */
+    activityPingIntervalSec: types.TUint16;
+
+    /**
+     * Remote server IP address for activity pings.
+     * The device acts as a TCP client and periodically
+     * connects to the specified host.
+     */
+    activityPingIp: types.TIpV4;
+
+    /**
+     * Remote server TCP port for activity pings.
+     * Used together with {@link activityPingIp}.
+     */
+    activityPingPort: types.TUint16;
+}
+
+export interface IGsmStatusAttributesV11 {
+    OPERATION_ERROR_PRESENT: boolean,
+    USING_METER_CONFIGURATION: boolean,
+    CONFIGURATION_DATA_SAVED: boolean,
+    DEVICE_LONG_ADDRESS_SET: boolean,
+    DEVICE_SHORT_ADDRESS_SET: boolean,
+    CONFIGURATION_VALID: boolean,
+    CONFIGURATION_SAVE_REQUIRED: boolean,
+    CONFIGURATION_READY: boolean,
+    READY_FOR_OPERATION: boolean
+}
+
+export interface IGsmStatusV11 {
+    hardwareVersion: types.IVersion,
+    softwareVersion: types.IVersion,
+    uptime: types.TUint32,
+    lastErrorStatus: types.TUint8,
+    INTCON1: types.TUint16,
+    attributes: IGsmStatusAttributesV11,
+
+    /**
+     * Number of allocated data blocks.
+     */
+    allocatedDataBlockCount: types.TUint8;
+
+    /**
+     * Number of allocated messages.
+     */
+    allocatedMessageCount: types.TUint8;
+
+    /**
+     * IP address assigned to the modem by the GPRS network.
+     */
+    ip: types.TIpV4;
+
+    /**
+     * GSM signal strength (RSSI).
+     */
+    rssi: types.TUint8;
+
+    /**
+     * GSM bit error rate (BER).
+     */
+    ber: types.TUint8;
+
+    /**
+     * Maximum number of TCP requests.
+     */
+    maxTcpRequestCount: types.TUint8;
+
+    /**
+     * Maximum number of requests to the mtx device.
+     */
+    maxMtxRequestCount: types.TUint8;
+
+    /**
+     * GSM module operational state.
+     */
+    gsmState: types.TUint8;
+
+    /**
+     * TCP stack operational state.
+     */
+    tcpState: types.TUint8;
+
+    /**
+     * MTX module operational state.
+     */
+    mtxState: types.TUint8;
+}
+
+export interface IGsmStatusAttributesV12 {
+    MESSAGE_SYSTEM_LOCKED: boolean,
+    OPERATION_ERROR_PRESENT: boolean,
+    SAVE_PARAMETERS_NOT_SUPPORTED: boolean,
+    USING_METER_CONFIGURATION: boolean,
+    CONFIGURATION_DATA_SAVED: boolean,
+    DEVICE_LONG_ADDRESS_SET: boolean,
+    DEVICE_SHORT_ADDRESS_SET: boolean,
+    DEVICE_CONFIGURATION_RECEIVED: boolean,
+    CONFIGURATION_SAVE_REQUIRED: boolean,
+    CONFIGURATION_VALID: boolean,
+    READY_FOR_OPERATION: boolean
+}
+
+export interface IGsmStatus12 {
+    hardwareVersion: types.IVersion,
+    softwareVersion: types.IVersion,
+    uptime: types.TUint32,
+
+    /**
+     * Number of allocated messages.
+     */
+    allocatedMessageCount: types.TUint8;
+
+    /**
+     * Number of allocated data blocks.
+     */
+    allocatedDataBlockCount: types.TUint8;
+
+    attributes: IGsmStatusAttributesV12,
+
+    /**
+     * IP address assigned to the modem by the GPRS network.
+     */
+    ip: types.TIpV4;
+
+    /**
+     * GSM signal strength (RSSI).
+     */
+    rssi: types.TUint8;
+
+    /**
+     * GSM bit error rate (BER).
+     */
+    ber: types.TUint8;
+
+    lastErrorStatus: types.TUint8,
+
+    INTCON1: types.TUint16,
+    INTCON2: types.TUint16,
+
+    lastErrorRCON: types.TUint16,
+    lastErrorINTTREG: types.TUint16,
+
+    /**
+     * MTX module operational state.
+     */
+    mtxState: types.TUint8;
+
+    /**
+     * GSM module operational state.
+     */
+    gsmState: types.TUint8;
+
+    /**
+     * TCP stack operational state.
+     */
+    tcpState: types.TUint8;
+
+    /**
+     * Maximum number of requests to the mtx device.
+     */
+    maxMtxRequestCount: types.TUint8;
+
+    mtxErrorCount: types.TUint8;
+
+    /**
+     * Maximum number of TCP requests.
+     */
+    maxTcpRequestCount: types.TUint16;
+
+    tcpErrorCount: types.TUint16;
+}
+
+export type TGsmStatus =
+    {version: 0x11, data: IGsmStatusV11} |
+    {version: 0x12, data: IGsmStatus12};
 
 /**
  * Day profiles, seasons and special days are grouped into a tariff plan.
@@ -1059,6 +1279,8 @@ export interface IGetDemandParametersResponseParameters {
 
 export const GSM_BLOCK_PREFIX = 0xda;
 export const GSM_BLOCK_SIZE = 60;
+export const GSM_VALID_BLOCK_NUMBER = gsmBlockTypes.STATUS + 1;
+export const GSM_CONFIGURATION_PREFIX = 0xae;
 export const TARIFF_PLAN_SIZE = 11;
 export const OPERATOR_PARAMETERS_SIZE = 74;
 export const SEASON_PROFILE_DAYS_NUMBER = 7;
@@ -1113,6 +1335,32 @@ const displaySetExtMask = {
     MAGNET_INDUCTION: 0x08000000,
     OPTOPORT_SPEED: 0x40000000,
     SORT_DISPLAY_SCREENS: 0x80000000
+};
+
+const gsmAttributeV11Mask = {
+    OPERATION_ERROR_PRESENT: 0x0001,
+    USING_METER_CONFIGURATION: 0x0002,
+    CONFIGURATION_DATA_SAVED: 0x0004,
+    DEVICE_LONG_ADDRESS_SET: 0x0008,
+    DEVICE_SHORT_ADDRESS_SET: 0x0010,
+    CONFIGURATION_VALID: 0x0020,
+    CONFIGURATION_SAVE_REQUIRED: 0x0040,
+    CONFIGURATION_READY: 0x0080,
+    READY_FOR_OPERATION: 0x0100
+};
+
+const gsmAttributeV12Mask = {
+    MESSAGE_SYSTEM_LOCKED: 0x0001,
+    OPERATION_ERROR_PRESENT: 0x0002,
+    SAVE_PARAMETERS_NOT_SUPPORTED: 0x0004,
+    USING_METER_CONFIGURATION: 0x0008,
+    CONFIGURATION_DATA_SAVED: 0x0010,
+    DEVICE_LONG_ADDRESS_SET: 0x0020,
+    DEVICE_SHORT_ADDRESS_SET: 0x0040,
+    DEVICE_CONFIGURATION_RECEIVED: 0x0080,
+    CONFIGURATION_SAVE_REQUIRED: 0x0100,
+    CONFIGURATION_VALID: 0x0200,
+    READY_FOR_OPERATION: 0x0400
 };
 
 const relaySet1Mask = {
@@ -1538,6 +1786,193 @@ export const setDateTime = function ( buffer: IBinaryBuffer, dateTime: IDateTime
     buffer.setUint8(dateTime.date as unknown as types.TUint8);
     buffer.setUint8(dateTime.month as unknown as types.TUint8);
     buffer.setUint8(dateTime.year as unknown as types.TUint8);
+};
+
+const gprsAccessPointSize: number = 27;
+const gprsUserNameSize: number = 14;
+const gprsPasswordSize: number = 14;
+
+
+export const getGsmConfiguration = ( buffer: IBinaryBuffer ): IGsmConfiguration => {
+    const configurationPrefix = buffer.getUint8();
+
+    if ( configurationPrefix !== GSM_CONFIGURATION_PREFIX ) {
+        throw new Error(`Gsm configuration. Invalid prefix: ${configurationPrefix}.`);
+    }
+
+    return {
+        blockVersion: buffer.getUint8(),
+        configurationId: buffer.getUint32(),
+        gsmAccessTypes: buffer.getUint8(),
+        gprsAccessPoint: buffer.getFixedString(gprsAccessPointSize),
+        gprsUserName: buffer.getFixedString(gprsUserNameSize),
+        gprsPassword: buffer.getFixedString(gprsPasswordSize),
+        commandServerPort: buffer.getUint16(),
+        activityServerPort: buffer.getUint16(),
+        activityPingIntervalSec: buffer.getUint16(),
+        activityPingIp: buffer.getIpV4(),
+        activityPingPort: buffer.getUint16()
+    };
+};
+
+export const setGsmConfiguration = ( buffer: IBinaryBuffer, value: IGsmConfiguration ) => {
+    buffer.setUint8(GSM_CONFIGURATION_PREFIX);
+    buffer.setUint8(value.blockVersion);
+    buffer.setUint32(value.configurationId);
+    buffer.setUint8(value.gsmAccessTypes);
+    buffer.setFixedString(value.gprsAccessPoint, gprsAccessPointSize);
+    buffer.setFixedString(value.gprsUserName, gprsUserNameSize);
+    buffer.setFixedString(value.gprsPassword, gprsPasswordSize);
+    buffer.setUint16(value.commandServerPort);
+    buffer.setUint16(value.activityServerPort);
+    buffer.setUint16(value.activityPingIntervalSec);
+    buffer.setIpV4(value.activityPingIp);
+    buffer.setUint16(value.activityPingPort);
+};
+
+export const getGsmStatus11 = ( buffer: IBinaryBuffer ): IGsmStatusV11 => ({
+    hardwareVersion: buffer.getVersion(),
+    softwareVersion: buffer.getVersion(),
+    uptime: buffer.getUint32(),
+    lastErrorStatus: buffer.getUint8(),
+    INTCON1: buffer.getUint16(),
+    attributes: (bitSet.toObject(gsmAttributeV11Mask, buffer.getUint32()) as unknown) as IGsmStatusAttributesV11,
+    allocatedDataBlockCount: buffer.getUint8(),
+    allocatedMessageCount: buffer.getUint8(),
+    ip: buffer.getIpV4(),
+    rssi: buffer.getUint8(),
+    ber: buffer.getUint8(),
+    maxTcpRequestCount: buffer.getUint8(),
+    maxMtxRequestCount: buffer.getUint8(),
+    gsmState: buffer.getUint8(),
+    tcpState: buffer.getUint8(),
+    mtxState: buffer.getUint8()
+});
+
+export const setGsmStatus11 = ( buffer: IBinaryBuffer, value: IGsmStatusV11 ) => {
+    buffer.setVersion(value.hardwareVersion);
+    buffer.setVersion(value.softwareVersion);
+    buffer.setUint32(value.uptime);
+    buffer.setUint8(value.lastErrorStatus);
+    buffer.setUint16(value.INTCON1);
+    buffer.setUint32(bitSet.fromObject(gsmAttributeV11Mask, (value.attributes as unknown) as bitSet.TBooleanObject));
+    buffer.setUint8(value.allocatedDataBlockCount);
+    buffer.setUint8(value.allocatedMessageCount);
+    buffer.setIpV4(value.ip);
+    buffer.setUint8(value.rssi);
+    buffer.setUint8(value.ber);
+    buffer.setUint8(value.maxTcpRequestCount);
+    buffer.setUint8(value.maxMtxRequestCount);
+    buffer.setUint8(value.gsmState);
+    buffer.setUint8(value.tcpState);
+    buffer.setUint8(value.mtxState);
+};
+
+export const getGsmStatus12 = ( buffer: IBinaryBuffer ): IGsmStatus12 => ({
+    hardwareVersion: buffer.getVersion(),
+    softwareVersion: buffer.getVersion(),
+    uptime: buffer.getUint32(),
+    allocatedMessageCount: buffer.getUint8(),
+    allocatedDataBlockCount: buffer.getUint8(),
+    attributes: (bitSet.toObject(gsmAttributeV12Mask, buffer.getUint32()) as unknown) as IGsmStatusAttributesV12,
+    ip: buffer.getIpV4(),
+    rssi: buffer.getUint8(),
+    ber: buffer.getUint8(),
+    lastErrorStatus: buffer.getUint8(),
+    INTCON1: buffer.getUint16(),
+    INTCON2: buffer.getUint16(),
+    lastErrorRCON: buffer.getUint16(),
+    lastErrorINTTREG: buffer.getUint16(),
+    mtxState: buffer.getUint8(),
+    gsmState: buffer.getUint8(),
+    tcpState: buffer.getUint8(),
+    maxMtxRequestCount: buffer.getUint8(),
+    mtxErrorCount: buffer.getUint8(),
+    maxTcpRequestCount: buffer.getUint16(),
+    tcpErrorCount: buffer.getUint16()
+});
+
+export const setGsmStatus12 = ( buffer: IBinaryBuffer, value: IGsmStatus12 ) => {
+    buffer.setVersion(value.hardwareVersion);
+    buffer.setVersion(value.softwareVersion);
+    buffer.setUint32(value.uptime);
+    buffer.setUint8(value.allocatedMessageCount);
+    buffer.setUint8(value.allocatedDataBlockCount);
+    buffer.setUint32(bitSet.fromObject(gsmAttributeV12Mask, (value.attributes as unknown) as bitSet.TBooleanObject));
+    buffer.setIpV4(value.ip);
+    buffer.setUint8(value.rssi);
+    buffer.setUint8(value.ber);
+    buffer.setUint8(value.lastErrorStatus);
+    buffer.setUint16(value.INTCON1);
+    buffer.setUint16(value.INTCON2);
+    buffer.setUint16(value.lastErrorRCON);
+    buffer.setUint16(value.lastErrorINTTREG);
+    buffer.setUint8(value.mtxState);
+    buffer.setUint8(value.gsmState);
+    buffer.setUint8(value.tcpState);
+    buffer.setUint8(value.maxMtxRequestCount);
+    buffer.setUint8(value.mtxErrorCount);
+    buffer.setUint16(value.maxTcpRequestCount);
+    buffer.setUint16(value.tcpErrorCount);
+};
+
+export const getGsmStatus = ( buffer: IBinaryBuffer ): TGsmStatus => {
+    const payloadLength = buffer.bytesLeft;
+
+    if ( payloadLength < 1 ) {
+        throw new Error(`GsmStatus. Invalid payload length: ${payloadLength}.`);
+    }
+
+    const version = buffer.getUint8();
+
+    switch (version) {
+        case 0x11: {
+            if ( payloadLength < 29 ) {
+                throw new Error(`GsmStatus. Invalid payload length: ${payloadLength}.`);
+            }
+
+            return {
+                version: 0x11,
+                data: getGsmStatus11(buffer)
+            };
+        }
+
+        case 0x12:
+        case 0x16: {
+            if ( payloadLength < 39 ) {
+                throw new Error(`GsmStatus. Invalid payload length: ${payloadLength}.`);
+            }
+
+            return {
+                version: 0x12,
+                data: getGsmStatus12(buffer)
+            };
+        }
+
+        default:
+            throw new Error(`GsmStatus. Unsupported version: ${version}.`);
+    }
+};
+
+export const setGsmStatus = ( buffer: IBinaryBuffer, {version, data}: TGsmStatus ) => {
+    switch ( version ) {
+        case 0x11: {
+            buffer.setUint8(version);
+            setGsmStatus11(buffer, data);
+            break;
+        }
+
+        case 0x12: {
+            buffer.setUint8(version);
+            setGsmStatus12(buffer, data);
+            break;
+        }
+
+        default: {
+            const exhaustive: never = version;
+            throw new Error(`GsmStatus. Unsupported version: ${String(exhaustive)}.`);
+        }
+    }
 };
 
 export const getGsmBlock = ( commandName: string, bytes: types.TBytes ): IGsmBlock => {
---
+++ src/mtx1/utils/gsmParameters.ts
@@ -0,0 +1,115 @@
+import * as types from '../types.js';
+import BinaryBuffer, {IBinaryBuffer} from '../../utils/BinaryBuffer.js';
+import {convertCrcToBytes} from '../../utils/payloadCrc16.js';
+import calculateCrc16 from '../../utils/calculateCrc16.js';
+import {
+    GSM_BLOCK_SIZE,
+    GSM_VALID_BLOCK_NUMBER,
+    getGsmConfiguration,
+    setGsmConfiguration,
+    getGsmStatus,
+    setGsmStatus,
+    IGsmBlock,
+    IGsmConfiguration,
+    TGsmStatus
+} from './CommandBinaryBuffer.js';
+import * as gsmBlockTypes from '../constants/gsmBlockTypes.js';
+import getHexFromBytes from '../../utils/getHexFromBytes.js';
+
+
+export type Parameters =
+    {type: 'configuration', data: IGsmConfiguration} |
+    {type: 'status', data: TGsmStatus} |
+    {type: 'incomplete'} |
+    {type: 'unsupported', block: IGsmBlock};
+
+export class Collector {
+    payload0?: types.TBytes;
+
+    payload1?: types.TBytes;
+
+    push ( block: IGsmBlock ): Parameters {
+        const [payloadLength, ...data] = block.data;
+        const payload = data.slice(0, payloadLength);
+        let buffer: IBinaryBuffer;
+
+        switch ( block.index ) {
+            case gsmBlockTypes.CONFIGURATION_0:
+                this.payload0 = payload;
+                break;
+
+            case gsmBlockTypes.CONFIGURATION_1:
+                this.payload1 = payload;
+                break;
+
+            case gsmBlockTypes.STATUS: {
+                buffer = new BinaryBuffer(payload);
+                console.log(`read gsmStatus: ${getHexFromBytes(payload)}`);
+
+                return {type: 'status', data: getGsmStatus(buffer)};
+            }
+
+            default:
+                return {type: 'unsupported', block};
+        }
+
+        if (!this.payload0 || !this.payload1) {
+            return {type: 'incomplete'};
+        }
+
+        const bytes = [...this.payload0, ...this.payload1];
+
+        this.clear();
+
+        buffer = new BinaryBuffer(bytes);
+
+        const configuration = getGsmConfiguration(buffer);
+        const calculatedCrc = calculateCrc16(buffer.getBytesToOffset());
+        const receivedCrc = buffer.getUint16();
+
+        if ( receivedCrc > 0 && calculatedCrc !== receivedCrc ) {
+            throw new Error('Gsm configuration. Invalid crc. '
+                + `Calculated: 0x${calculatedCrc.toString(16).padStart(4, '0')}, `
+                + `received: ${receivedCrc.toString(16).padStart(4, '0')}.`);
+        }
+
+        return {type: 'configuration', data: configuration};
+    }
+
+    clear () {
+        this.payload0 = null;
+        this.payload1 = null;
+    }
+}
+
+export const split = ( parameters: Parameters ): Array<IGsmBlock> => {
+    const buffer: IBinaryBuffer = new BinaryBuffer(GSM_BLOCK_SIZE * GSM_VALID_BLOCK_NUMBER);
+
+    switch ( parameters.type ) {
+        case 'configuration': {
+            setGsmConfiguration(buffer, parameters.data);
+
+            const bytes = buffer.getBytesToOffset();
+            const payload0 = bytes.slice(0, GSM_BLOCK_SIZE);
+            const payload1 = bytes.slice(GSM_BLOCK_SIZE);
+            const crcBytes = convertCrcToBytes(calculateCrc16(bytes));
+
+            return [
+                {index: 0, data: [payload0.length, ...payload0]},
+                {index: 1, data: [payload1.length, ...payload1, ...crcBytes]}
+            ];
+        }
+
+        case 'status': {
+            setGsmStatus(buffer, parameters.data);
+
+            const payload = buffer.getBytesToOffset();
+            console.log(`write GsmStatus: ${getHexFromBytes(payload)}`);
+
+            return [{index: 3, data: [payload.length, ...payload]}];
+        }
+
+        default:
+            return [];
+    }
+};
