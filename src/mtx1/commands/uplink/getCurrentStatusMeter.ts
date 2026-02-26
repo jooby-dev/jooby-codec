2a82a8b890d6cd977d34e3290a6262f0f6a88391
Stanislav Kalashnik <darkpark.main@gmail.com>
2026-02-26 19:19:00 +0200

    fix: update getCurrentStatusMeter parameters (tbadUnequalCurrAll) and test cases

--- src/mtx1/commands/uplink/getCurrentStatusMeter.ts
+++ src/mtx1/commands/uplink/getCurrentStatusMeter.ts
@@ -25,6 +25,7 @@
  *     tbadVAVB: 34567,
  *     tbadImaxAll: 956726,
  *     tbadPmaxAll: 340,
+ *     tbadUnequalCurrAll: 1234,
  *     tbadFREQ: 436,
  *     relayStatus: {
  *         RELAY_STATE: true,
@@ -100,6 +101,9 @@ interface IGetCurrentStatusMeterResponseParameters {
 
     /** Duration of maximum power during the accounting period, in seconds. */
     tbadPmaxAll: types.TUint32,
+
+    /** Duration of current imbalance over the calculation period, in seconds. */
+    tbadUnequalCurrAll: types.TUint32,
 
     /** Duration of frequency deviation from normal during the accounting period, in seconds. */
     tbadFREQ: types.TUint32,
@@ -184,6 +188,7 @@ export const examples: command.TCommandExamples = {
             tbadVAVB: 34567,
             tbadImaxAll: 956726,
             tbadPmaxAll: 340,
+            tbadUnequalCurrAll: 1234,
             tbadFREQ: 436,
             relayStatus: {
                 RELAY_STATE: true,
@@ -229,7 +234,7 @@ export const examples: command.TCommandExamples = {
         bytes: [
             0x39, 0x1f,
             0x00, 0x01, 0x22, 0x50, 0x00, 0x00, 0x87, 0x07, 0x00, 0x0e, 0x99, 0x36, 0x00, 0x00, 0x01, 0x54,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xb4, 0x61, 0x85, 0x10, 0x01, 0x01, 0x03, 0x01
+            0x00, 0x00, 0x04, 0xd2, 0x00, 0x00, 0x01, 0xb4, 0x61, 0x85, 0x10, 0x01, 0x01, 0x03, 0x01
         ]
     }
 };
@@ -250,10 +255,7 @@ export const fromBytes = ( bytes: types.TBytes ): IGetCurrentStatusMeterResponse
     const tbadVAVB = buffer.getUint32();
     const tbadImaxAll = buffer.getUint32();
     const tbadPmaxAll = buffer.getUint32();
-
-    // reserved
-    buffer.getUint32();
-
+    const tbadUnequalCurrAll = buffer.getUint32();
     const tbadFREQ = buffer.getUint32();
     const relayStatus = bitSet.toObject(extendedCurrentValues2RelayStatusMask, buffer.getUint8()) as unknown as IExtendedCurrentValues2RelayStatus;
     const statusEvent1 = buffer.getUint8();
@@ -273,6 +275,7 @@ export const fromBytes = ( bytes: types.TBytes ): IGetCurrentStatusMeterResponse
         tbadVAVB,
         tbadImaxAll,
         tbadPmaxAll,
+        tbadUnequalCurrAll,
         tbadFREQ,
         relayStatus,
         statusEvent: (bitSet.toObject(eventStatusMask, statusEventValue) as unknown) as IEventStatus,
@@ -298,10 +301,7 @@ export const toBytes = ( parameters: IGetCurrentStatusMeterResponseParameters ):
     buffer.setUint32(parameters.tbadVAVB);
     buffer.setUint32(parameters.tbadImaxAll);
     buffer.setUint32(parameters.tbadPmaxAll);
-
-    // reserved
-    buffer.setUint32(0);
-
+    buffer.setUint32(parameters.tbadUnequalCurrAll);
     buffer.setUint32(parameters.tbadFREQ);
     buffer.setUint8(bitSet.fromObject(extendedCurrentValues2RelayStatusMask, (parameters.relayStatus as unknown) as bitSet.TBooleanObject));
     buffer.setUint8(statusEventValue & 0xff);
