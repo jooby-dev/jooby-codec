40c737656f3cf68c5ab00a8e0a4452607a325479
p.poukh <p.poukh@infomir.com>
2026-02-27 14:25:45 +0200

    fix(mtx3): update getCurrentStatusMeter relay status

--- src/mtx3/commands/uplink/getCurrentStatusMeter.ts
+++ src/mtx3/commands/uplink/getCurrentStatusMeter.ts
@@ -72,6 +72,47 @@ import {getCurrentStatusMeter as commandId} from '../../constants/uplinkIds.js';
 import commandNames from '../../constants/uplinkNames.js';
 
 
+interface IRelayStatus {
+    /** Current relay state: 1 - on, 0 - off. */
+    RELAY_STATE: boolean,
+
+    /** Relay turned off due to poor voltage. */
+    RELAY_UBAD: boolean,
+
+    /** Relay turned off due to exceeding allowable power in net balance limit mode. */
+    RELAY_SALDO_OFF_ON_MAX_POWER: boolean,
+
+    /** Relay switched off from the control center. */
+    RELAY_OFF_CENTER: boolean,
+
+    /** Relay turned off due to maximum current. */
+    RELAY_IMAX: boolean,
+
+    /** Relay turned off due to maximum power. */
+    RELAY_PMAX: boolean
+}
+
+interface IRelayStatus2 {
+    /** Relay turned off due to cos φ. */
+    RELAY_COSFI: boolean,
+
+    /** Relay turned off due to balance. */
+    RELAY_SALDO_OFF_FLAG: boolean,
+
+    /** Relay turned off due to magnet interference. */
+    RELAY_MAGNET_OFF: boolean,
+
+    RELAY_HARD_STATE1: boolean,
+    RELAY_HARD_STATE2: boolean,
+    RELAY_HARD_STATE3: boolean,
+
+    /** Relay turned off due to maximum negative power exceeded. */
+    RELAY_P_MINUS_MAX_OFF: boolean
+
+    /** Relay turned off due to maximum reactive power exceeded. */
+    RELAY_P_REACTIVE_MAX: boolean
+}
+
 interface ITariffs {
     /**
      * Tariff for the group of consumed energies (`1st` grid).
@@ -121,16 +162,19 @@ interface IGetCurrentStatusMeterResponseParameters {
     tbadPMAXAll: types.TUint32;
 
     /**
+     * Relay status 2.
+     */
+    relayStatus2: IRelayStatus2,
+
+    /**
      * Duration of frequency deviation from normal during the billing period, in seconds.
      */
     tbadFREQ: types.TUint32;
 
     /**
-     * Relay state.
-     *
-     * `true` - on
+     * Relay status.
      */
-    relayStatus: boolean;
+    relayStatus: IRelayStatus;
 
     /**
      * Critical event status.
@@ -161,6 +205,27 @@ interface IGetCurrentStatusMeterResponseParameters {
 }
 
 
+const relayStatusMask = {
+    RELAY_STATE: 2 ** 0,
+    RELAY_UBAD: 2 ** 1,
+    RELAY_SALDO_OFF_ON_MAX_POWER: 2 ** 4,
+    RELAY_OFF_CENTER: 2 ** 5,
+    RELAY_IMAX: 2 ** 6,
+    RELAY_PMAX: 2 ** 7
+};
+
+const relayStatus2Mask = {
+    RELAY_COSFI: 2 ** 0,
+    RELAY_SALDO_OFF_FLAG: 2 ** 1,
+    RELAY_MAGNET_OFF: 2 ** 2,
+    RELAY_HARD_STATE_1: 2 ** 3,
+    RELAY_HARD_STATE_2: 2 ** 4,
+    RELAY_HARD_STATE_3: 2 ** 5,
+    RELAY_P_MINUS_MAX_OFF: 2 ** 6,
+    RELAY_P_REACTIVE_MAX: 2 ** 7
+};
+
+
 export const id: types.TCommandId = commandId;
 export const name: types.TCommandName = commandNames[commandId];
 export const headerSize = 2;
@@ -183,8 +248,25 @@ export const examples: command.TCommandExamples = {
             tbadVCAll: 67890,
             tbadIMAXAll: 956726,
             tbadPMAXAll: 340,
+            relayStatus2: {
+                RELAY_COSFI: false,
+                RELAY_SALDO_OFF_FLAG: false,
+                RELAY_MAGNET_OFF: false,
+                RELAY_HARD_STATE_1: false,
+                RELAY_HARD_STATE_2: false,
+                RELAY_HARD_STATE_3: false,
+                RELAY_P_MINUS_MAX_OFF: false,
+                RELAY_P_REACTIVE_MAX: false
+            },
             tbadFREQ: 436,
-            relayStatus: true,
+            relayStatus: {
+                RELAY_STATE: true,
+                RELAY_UBAD: false,
+                RELAY_SALDO_OFF_ON_MAX_POWER: false,
+                RELAY_OFF_CENTER: false,
+                RELAY_IMAX: false,
+                RELAY_PMAX: false
+            },
             statusEvent: {
                 CASE_OPEN: true,
                 MAGNETIC_ON: false,
@@ -252,12 +334,14 @@ export const fromBytes = ( bytes: types.TBytes ): IGetCurrentStatusMeterResponse
     const tbadVCAll = buffer.getUint32();
     const tbadIMAXAll = buffer.getUint32();
     const tbadPMAXAll = buffer.getUint32();
+    const relayStatus2 = bitSet.toObject(relayStatus2Mask, buffer.getUint8()) as unknown as IRelayStatus2;
 
-    // reserved
-    buffer.getUint32();
+    // reserved bytes
+    buffer.getUint8();
+    buffer.getUint16();
 
     const tbadFREQ = buffer.getUint32();
-    const relayStatus = !!(buffer.getUint8() & 1);
+    const relayStatus = bitSet.toObject(relayStatusMask, buffer.getUint8()) as unknown as IRelayStatus;
     const statusEvent1 = buffer.getUint8();
     const centerAlert = !!(buffer.getUint8() & 1);
     const calEnableFlag = !!(buffer.getUint8() & 1);
@@ -279,6 +363,7 @@ export const fromBytes = ( bytes: types.TBytes ): IGetCurrentStatusMeterResponse
         tbadVCAll,
         tbadIMAXAll,
         tbadPMAXAll,
+        relayStatus2,
         tbadFREQ,
         relayStatus,
         statusEvent: (bitSet.toObject(eventStatusMask, statusEventValue) as unknown) as IEventStatus,
@@ -307,12 +392,14 @@ export const toBytes = ( parameters: IGetCurrentStatusMeterResponseParameters ):
     buffer.setUint32(parameters.tbadVCAll);
     buffer.setUint32(parameters.tbadIMAXAll);
     buffer.setUint32(parameters.tbadPMAXAll);
+    buffer.setUint8(bitSet.fromObject(relayStatus2Mask, (parameters.relayStatus2 as unknown) as bitSet.TBooleanObject));
 
-    // reserved
-    buffer.setUint32(0);
+    // reserved bytes
+    buffer.setUint8(0);
+    buffer.setUint16(0);
 
     buffer.setUint32(parameters.tbadFREQ);
-    buffer.setUint8(parameters.relayStatus ? 1 : 0);
+    buffer.setUint8(bitSet.fromObject(relayStatusMask, (parameters.relayStatus as unknown) as bitSet.TBooleanObject));
     buffer.setUint8(statusEventValue & 0xff);
     buffer.setUint8(parameters.centerAlert ? 1 : 0);
     buffer.setUint8(parameters.calEnableFlag ? 1 : 0);
