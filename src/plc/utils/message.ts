3bd6b2cda02850480413b79fd333e1540c508e16
p.poukh <p.poukh@infomir.com>
2026-03-13 11:35:13 +0100

    fix(plc): use encryption only on MTX related subsystems

--- src/plc/utils/message.ts
+++ src/plc/utils/message.ts
@@ -1,21 +1,27 @@
-import * as types from '../types.js';
-import * as block from './block.js';
-import * as message from '../message/index.js';
 import {IToBytesOptions, IFromBytesOptions} from '../../mtx1/message/wrappers.js';
 import {TCommand} from '../../mtx1/utils/command.js';
+import * as accessLevelIds from '../../mtx1/constants/accessLevels.js';
+import * as types from '../types.js';
+import * as message from '../message/index.js';
+import * as subsystemIds from '../constants/subsystemIds.js';
+import * as block from './block.js';
 
 
 export const messageFromBlock = ( value: block.ISubsystemBlock, config: IFromBytesOptions = {} ) => {
+    const subsystemsWithEncryption = [subsystemIds.MTX, subsystemIds.MTX_REPORT, subsystemIds.MTX_EVENT];
     const direction = value.isDownlink
         ? message.downlink
         : message.uplink;
     const subsystemId = block.getCommandsSystemId(value);
 
     const fromBytes = direction.messageFromBytes('mtx1', subsystemId);
+    const accessLevel = subsystemsWithEncryption.includes(value.subsystemId)
+        ? value.accessLevel
+        : accessLevelIds.UNENCRYPTED;
 
     return fromBytes != null
         ? fromBytes(
-            value.accessLevel,
+            accessLevel,
             value.messageId,
             value.payload,
             config
